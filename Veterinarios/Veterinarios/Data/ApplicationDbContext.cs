85afa85d80176fab79e826074ddd542a84c01039
jcnpereira <casimiro@ipt.pt>
2022-03-28 15:31:39 +0100

    defining classes to connect to database tables

--- Veterinarios/Veterinarios/Data/ApplicationDbContext.cs
+++ Veterinarios/Veterinarios/Data/ApplicationDbContext.cs
@@ -1,10 +1,21 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
+using Veterinarios.Models;
+
 namespace Veterinarios.Data {
    public class ApplicationDbContext : IdentityDbContext {
       public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
           : base(options) {
       }
+
+
+      // define table on the database
+      public DbSet<Animal> Animals { get; set; }
+      public DbSet<Vet> Vets { get; set; }
+      public DbSet<Appointement> Appointements { get; set; }
+      public DbSet<Owner> Owners { get; set; }
+
+
    }
 }
