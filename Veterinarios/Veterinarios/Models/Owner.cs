2890880bcb445f4d72fa5e90c2fa1b27ebd0ea34
jcnpereira <casimiro@ipt.pt>
2022-04-04 17:28:39 +0100

    Add a new atribute to Owners class

--- Veterinarios/Veterinarios/Models/Owner.cs
+++ Veterinarios/Veterinarios/Models/Owner.cs
@@ -21,17 +21,17 @@ namespace Veterinarios.Models {
       /// name of owner's animal
       /// </summary>
       [Required(ErrorMessage = "O {0} é de preenchimento obrigatório.")]
-      [StringLength(20, ErrorMessage = "O {0} não pode ter mais do que 20 carateres.")]   
-     [Display(Name ="Nome")]
-     [RegularExpression("[A-ZÂÓÍa-záéíóúàèìòùâêîôûãõäëïöüñç '-]+", ErrorMessage ="Só pode escrever letras no {0}")]
+      [StringLength(30, ErrorMessage = "O {0} não pode ter mais do que {1} carateres.")]
+      [Display(Name = "Nome")]
+      [RegularExpression("[A-ZÂÓÍa-záéíóúàèìòùâêîôûãõäëïöüñç '-]+", ErrorMessage = "Só pode escrever letras no {0}")]
       public string Name { get; set; }
 
       /// <summary>
       /// vat number
       /// </summary>
       [Required(ErrorMessage = "O {0} é de preenchimento obrigatório.")]
-      [StringLength(9, MinimumLength =9 , ErrorMessage = "O {0} tem de ter 9 carateres.")]
-     [RegularExpression("[123578][0-9]{8}",ErrorMessage ="O {0} deve começar por 1,2,3,5,7,8 seguido de 8 digitos numéricos.")]
+      [StringLength(9, MinimumLength = 9, ErrorMessage = "O {0} tem de ter 9 carateres.")]
+      [RegularExpression("[123578][0-9]{8}", ErrorMessage = "O {0} deve começar por 1,2,3,5,7,8 seguido de 8 digitos numéricos.")]
       public string NIF { get; set; }
 
       /// <summary>
@@ -40,8 +40,15 @@ namespace Veterinarios.Models {
       /// </summary>
       [StringLength(1, ErrorMessage = "O {0} não pode ter mais do que 1 caráter.")]
       [Display(Name = "Sexo")]
-      [RegularExpression("[MmFf]",ErrorMessage ="Só pode usar F, ou M, no campo {0}")]
+      [RegularExpression("[MmFf]", ErrorMessage = "Só pode usar F, ou M, no campo {0}")]
       public string Sex { get; set; }
+
+      /// <summary>
+      /// Email
+      /// </summary>
+      [EmailAddress(ErrorMessage ="Escreva um {0} válido, pf.")]
+      public string Email { get; set; }
+
 
       /// <summary>
       /// set of owner's animals
